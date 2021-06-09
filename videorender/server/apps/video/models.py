9f73769dd7842b81b11725cdca49753d9fb16638
ahousetov <ahousetov@gmail.com>
2021-06-09 15:04:09 +0300

    Add video model

--- videorender/server/apps/video/models.py
+++ videorender/server/apps/video/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from .validators import video_extension_validator
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+
+class Video(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    video = models.FileField(upload_to='video',
+                             max_length=255,
+                             blank=False,
+                             validators=[video_extension_validator])
+
+    def __str__(self):
+        return self.title
+
